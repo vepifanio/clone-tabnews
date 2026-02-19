569ee6fd3010f11e35cce70e211eda02e524c794
vepifanio <vepifanio1@gmail.com>
2026-02-19 11:34:18 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Krystal, vamos fazer um bonequinho chamado Bernardo!</h1>
+  return <h1>Krystal, vamos fazer um bonequinho chamado Bernardo!</h1>;
 }
 
 export default Home;
